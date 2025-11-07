99be4a46c6f1a55aae92200c9f45ad2c7ff65b27
Your Name <40523890+Brain-dawg@users.noreply.github.com>
2025-11-06 22:11:06 -0500

    revert

--- devserver_config/vpi/vpi_mge.py
+++ devserver_config/vpi/vpi_mge.py
@@ -50,57 +50,6 @@ class Encoder(json.JSONEncoder):
 		else:
 			return super().default(o)
 
-# Emulate behavior of modulus in C / Squirrel
-def mod(a, b):
-	return (a % b + b) % b
-
-# Simple encryption algorithm based on timestamp, time, and a key
-def Encrypt(string):
-	timestamp = int(round(time.time()))
-
-	# Add a bit of randomness
-	t = mod(timestamp, 1024)        # Sin doesn't give good output for large values, keep things small
-	f = math.fabs(math.sin(16 * t)) # Give our time a bit of variance
-	h = math.floor(f * 127 + 0.5)   # Get a hash value from 0 - 127 (really this could be any number though)
-
-	# Initialization vector to provide true randomness since we always use the same key
-	# Without this the output tends to repeat quite often
-	iv = ""
-	for ch in string:
-		iv += chr(randint(35, 126))
-
-	enc = ""
-	for i, ch in enumerate(string):
-		key_index = mod(i, len(SECRET)) # Corresponding index in our key, loop if necessary
-		key_char  = SECRET[key_index]
-
-		# Encode the character; shifted using hash and key_char; limited to 32 - 127 ASCII
-		enc += chr(32 + mod(ord(ch) + h + ord(iv[i]) + ord(key_char), 95))
-
-	return {
-		"enc"       : enc,
-		"iv"        : iv,
-		"timestamp" : timestamp,
-		"ticks"     : 0,
-	}
-
-# Decryption
-def Decrypt(enc, iv, timestamp, ticks):
-	t = mod(timestamp + ticks, 1024)
-	f = math.fabs(math.sin(16 * t))
-	h = math.floor(f * 127 + 0.5)
-
-	dec = ""
-	for i, ch in enumerate(enc):
-		key_index = mod(i, len(SECRET))
-		key_char  = SECRET[key_index]
-
-		dec_char = mod(ord(ch) - 32 - h - ord(iv[i]) - ord(key_char), 95)
-		if (dec_char < 32):
-			dec_char += 95 * math.ceil((32 - dec_char) / 95.0)
-		dec += chr(dec_char)
-
-	return dec
 
 # Grab the hostname from a path
 def GetHostname(path):
@@ -111,7 +60,6 @@ def GetHostname(path):
 	return host[:sep]
 
 # Write responses from interface functions to file
-MAX_FILE_SIZE = 16000
 def WriteCallbacksToFile():
 	# Hosts to delete
 	delete = []
@@ -131,36 +79,12 @@ def WriteCallbacksToFile():
 			# Wipe the file
 			f.truncate(0)
 
-			table = {"Calls": info}
-			table["Identity"] = Encrypt(SECRET)
-
-			string   = json.dumps(table, cls=Encoder)
+			table	 = {"Calls": info}
 			overflow = {}
 
-			if (len(string) >= MAX_FILE_SIZE):
-				# Sort responses by size
-				cbs = [[token, response] for token, response in info.items()]
-				for l in cbs: l.append(len(json.dumps(l)))
-				cbs.sort(key=lambda l: l[2])
-
-				# Loop through and get as many as can fit
-				totalsize = 0
-				fits = {}
-				for l in cbs:
-					token, response, size = l
-
-					# Client expects error responses to start with [VPI ERROR]
-					if (size >= MAX_FILE_SIZE):
-						response = "[VPI ERROR] (token) :: Response size exceeds maximum"
-						size = len(json.dumps(response, cls=Encoder))
-
-					if (totalsize + size < MAX_FILE_SIZE):
-						totalsize   += size
-						fits[token] =  response
-					else:
-						overflow[token] = response
-
-				table["Calls"] = fits
+			# Find the number of responses we can write fitting into the max VScript readable file size of 16kb
+			string = None
+			while (True):
 				string = json.dumps(table, cls=Encoder)
 				strlen = len(string)
 
@@ -191,7 +115,6 @@ def WriteCallbacksToFile():
 
 			if (not len(string)): continue
 
-			LOGGER.info(f"WriteCallbacksToFile: [{host}] {len(string)} bytes")
 			f.write(string)
 
 	for host in delete:
@@ -210,25 +133,22 @@ async def ExecCalls():
 			try:
 				func = getattr(vpi_interfaces, func)
 				result = await func(call, POOL)
-			except Exception as e:
-				LOGGER.error(f"Error in ExecCallChain: {e}")
+			except:
 				continue
 
-		LOGGER.info(f"ExecCallChain: [{host}] {func}")
 		return result
 
 	# Prepare calls
 	for host, table in calls.items():
 		for call in table["async"]:
 			func = call["func"]
-			LOGGER.info(f"ExecCalls: [{host}] {func}")
 			if (not func.startswith("VPI_")): continue
 			try:
 				func = getattr(vpi_interfaces, func)
 				tasks.append(func(call, POOL))
 				contexts.append({"host":host, "call":call})
 			except Exception as e:
-				LOGGER.error(f"Error in ExecCalls: {e}")
+				LOGGER.error(e)
 
 		for call_chain in table["chain"]:
 			if (not len(call_chain)): continue
@@ -251,29 +171,23 @@ async def ExecCalls():
 			callbacks[host][token] = result
 
 def ExtractCallsFromFile(path):
+	try:
+		with open(path, "r+") as f:
+			contents = f.read()
+			if (contents.endswith("\x00")):
+				contents = contents[:-1]
 
-	# try:
-	with open(path, "r+") as f:
-		contents = f.read()
-		if (contents.endswith("\x00")):
-			contents = contents[:-1]
+			data = json.loads(contents)
 
-		data = json.loads(contents)
+			host = GetHostname(path)
+			if (host not in calls):
+				calls[host] = {"async":[], "chain":[]}
 
-		ident = Decrypt(**data["Identity"])
-		if (ident != SECRET and not BYPASS_SECRET):
-			LOGGER.error(f"Error in ExtractCallsFromFile: Invalid identification in file: {path}; ignoring")
-			return
+			calls[host]["async"].extend(data["Calls"]["async"])
+			calls[host]["chain"].extend(data["Calls"]["chain"])
 
-		host = GetHostname(path)
-		if (host not in calls):
-			calls[host] = {"async":[], "chain":[]}
-
-		calls[host]["async"].extend(data["Calls"]["async"])
-		calls[host]["chain"].extend(data["Calls"]["chain"])
-
-	# except Exception as e:
-		# LOGGER.error(f"Invalid input received from client in: \"{path}\"")
+	except Exception as e:
+		LOGGER.error(f"Invalid input received from client in: \"{path}\"")
 
 async def main():
 
@@ -286,8 +200,8 @@ async def main():
 
 	# Watchdog loop
 	while True:
-
 		time.sleep(0.2)
+		current_time = time.time()
 
 		# Watch for changes to vpi_interfaces and reload the module if necessary
 		last_modtime = os.path.getmtime("vpi_interfaces.py")
@@ -304,7 +218,6 @@ async def main():
 
 
 		files = os.listdir(SCRIPTDATA_DIR)
-		LOGGER.info(f"files: {files}")
 
 		for file in files:
 			path = os.path.join(SCRIPTDATA_DIR, file)
